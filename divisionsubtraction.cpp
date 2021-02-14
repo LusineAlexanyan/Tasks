e5efbcd13878585d245eba5885472dcdfd7edc15
LusineAlexanyan <78852634+LusineAlexanyan@users.noreply.github.com>
2021-02-14 16:59:41 +0400

    Add files via upload

---
+++ divisionsubtraction.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int DemoConsoleOutput();
+
+int main()
+{
+    DemoConsoleOutput();
+    return 0;
+}
+
+int DemoConsoleOutput()
+{
+    cout << "Performing division 20 / 4 = " << 20 / 4 << endl;
+    cout << "Performing subtraction 20 - 4 = " << 20 - 4 << endl;
+    return 0;
+}
